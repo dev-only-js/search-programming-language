7b5406d7d16f7d8047117ec28b53b06759c2a7d8
adultlee <seoungin1228@gmail.com>
2022-09-26 21:13:23 +0900

    feat: suggestion 추가

--- App.js
+++ App.js
@@ -1,23 +1,54 @@
 import SearchInput from "./SearchInput.js";
+import Suggestion from "./Suggestion.js";
 
-import { fetchLanguage } from "./api.js";
+import { fetchLanguages } from "./api.js";
 
 export default function App({ $target }) {
   this.state = {
-    fetchedLanguage: [],
-    selectedLanguage: [],
+    fetchedLanguages: [],
+    selectedLanguages: [],
   };
 
-  //   this.setState = () = { // nextState
-  //     // todo
-  //   };
+  this.setState = (nextState) => {
+    // nextState
+    this.state = {
+      fetchedLanguages: [...nextState.fetchedLanguages],
+      selectedLanguages: [...nextState.selectedLanguages],
+    }; // 이거 문법 익히기
+
+    // this.state = {
+    //   ...this.state,
+    //   ...nextState,
+    // };
+
+    suggestion.setState({
+      items: this.state.fetchedLanguages,
+    });
+  };
 
   const searchInput = new SearchInput({
     $target: $target,
     initialState: "",
     onChange: async (keyword) => {
-      const language = await fetchLanguage(keyword);
-      console.log(language);
+      if (keyword.length === 0) {
+        this.setState({
+          fetchedLanguages: [],
+          selectedLanguages: [],
+        });
+      } else {
+        const languages = await fetchLanguages(keyword);
+        this.setState({
+          fetchedLanguages: languages,
+          selectedLanguages: [],
+        });
+      }
+    },
+  });
+
+  const suggestion = new Suggestion({
+    $target,
+    initialState: {
+      items: [],
     },
   });
 }
--- api.js
+++ api.js
@@ -1,19 +1,18 @@
 export const API_END_POINT =
-  "https://wr4a6p937i.execute-api.ap-northeast-2.amazonaws.com/dev";
+  "https://wr4a6p937i.execute-api.ap-northeast-2.amazonaws.com/dev"; // 해당 api를 저장합니다.
 
 const request = async (url) => {
-  const res = await fetch(url);
+  const res = await fetch(url); // fetch로 받은 url을 통해 받은 return 값을 저장합니다.
 
   if (res.ok) {
     const json = await res.json();
-    console.log(json);
     return json;
   }
 
   throw new Error("요청에 실패함");
 };
 
-export const fetchLanguage = async (keyword) => {
+export const fetchLanguages = async (keyword) => {
   return request(`${API_END_POINT}/languages?keyword=${keyword}`); // return 이 없었지만 return 을 추가해 줌으로 값을 더해준다.
 };
 
