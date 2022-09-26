d50ff769cb91e799e6eda030807221c54d3c6c07
adultlee <seoungin1228@gmail.com>
2022-09-26 19:24:01 +0900

    init

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
-console.log("hi");
+import SearchInput from "./SearchInput.js";
+
+export default function App({ $target }) {
+  this.state = {
+    fetchedLanguage: [],
+    selectedLanguage: [],
+  };
+
+  //   this.setState = () = { // nextState
+  //     // todo
+  //   };
+
+  const searchInput = new SearchInput({ $target, initialState: "" });
+}
